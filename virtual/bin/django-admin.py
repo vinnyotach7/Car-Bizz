e7a9be99221f627d98a1ea70efbe9ad047e501d2
vinnyotach7 <vinnyotach7@gmail.com>
2019-01-11 11:18:29 +0300

    this folder contains the environment used for the application

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/moringaschool/Documents/moringa-school-projects/django projects/car-pooler/virtual/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
